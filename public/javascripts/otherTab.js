02c665f24d28d48fdaa420711339fd4c7d156525
Yuefan Liu <lyvonne1011@gmail.com>
2017-03-26 00:12:41 -0700

    theme color change

--- public/javascripts/otherTab.js
+++ public/javascripts/otherTab.js
@@ -4,8 +4,10 @@
 function updateRotationDegree() {
     var inputVal = $('#inputDegree').val();
     if (inputVal < 0 || inputVal > 720) {
-        $('#rotationModal .modal-body > span').html('Angle should be in the range 0~720 degrees.');
+        $('#rotationModal .modal-body > span').html(inputVal + ' degree is not valid. ' +
+            'Angle should be in the range 0~720 degrees.');
         $('#rotationModal').modal();
+        $('#inputDegree').val("360")
     } else {
         $('#rotationImg').mouseover(function() {
             $(this).css('-webkit-transform', 'rotate(' + inputVal + 'deg)');
@@ -23,8 +25,10 @@ function updateRotationDegree() {
 function updateRotationTime() {
     var inputVal = $('#inputTime').val();
     if (inputVal < 0 || inputVal > 5) {
-        $('#rotationModal .modal-body > span').html('Time should be in the range 0~5s.');
+        $('#rotationModal .modal-body > span').html(inputVal + 's is not valid. ' +
+            'Time should be in the range 0~5s.');
         $('#rotationModal').modal();
+        $('#inputTime').val("1")
     } else {
         $('#rotationImg').css('-webkit-transition', '-webkit-transform ' + inputVal + 's ease-in-out');
         $('#rotationImg').css('transition', 'transform ' + inputVal + 's ease-in-out');
